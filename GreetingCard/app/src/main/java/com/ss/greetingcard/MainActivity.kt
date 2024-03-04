fa4a30f232ddef8a0fd780974b4129518ab26d12
Saranya Sankaranarayanan <2827393s@student.gla.ac.uk>
2024-03-04 16:07:08 +0000

    Greeting card example

--- GreetingCard/app/src/main/java/com/ss/greetingcard/MainActivity.kt
+++ GreetingCard/app/src/main/java/com/ss/greetingcard/MainActivity.kt
@@ -3,13 +3,17 @@ package com.ss.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ss.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +35,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(5.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
